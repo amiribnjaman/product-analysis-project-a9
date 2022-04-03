6100cd614cad707afd27d95d16effb9519c2bf53
amiribnjaman <amiribnjaman@gmail.com>
2022-04-04 01:36:15 +0600

    Home page banner section done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import watch1 from '../../assets/img/watch1.jpg';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            thsi is home
+        <div className='container my-5'>
+            <div className='row d-flex align-items-center'>
+                <div className='col-md-6'>
+                    <h1>WANNA Hand Watch</h1>
+                    <h2>Your satisfaction is Our Goal.</h2>
+                    <p>We are internet based an e-commerce business company. Our main product is hand watch. Last 5 years you services with 100% clients satisfaction. We pioritised our clients satisfaction, its our main objective.</p>
+                    <button>Explore More</button>
+                </div>
+                <div className='col-md-6 ps-5'>
+                    <img src={watch1} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
