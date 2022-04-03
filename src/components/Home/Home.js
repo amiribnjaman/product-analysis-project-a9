c9500455eaaa66dd90c64a606c9ee75426175775
amiribnjaman <amiribnjaman@gmail.com>
2022-04-04 01:58:07 +0600

    Custom hook created

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import watch1 from '../../assets/img/watch1.jpg';
+import ReviewsHomePage from '../ReviewsHomePage/ReviewsHomePage';
 import './Home.css';
 
 const Home = () => {
     return (
         <div className='container my-5'>
             <div className='row d-flex align-items-center'>
-                <div className='col-md-6'>
+                <div className='col-md-6 banner'>
                     <h1>WANNA Hand Watch</h1>
                     <h2>Your satisfaction is Our Goal.</h2>
                     <p>We are internet based an e-commerce business company. Our main product is hand watch. Last 5 years you services with 100% clients satisfaction. We pioritised our clients satisfaction, its our main objective.</p>
@@ -16,6 +17,7 @@ const Home = () => {
                     <img src={watch1} alt="" />
                 </div>
             </div>
+            <ReviewsHomePage />
         </div>
     );
 };
---
+++ src/components/Review/Review.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Review = (props) => {
+    const {name, review, image, rating} = props.review
+    console.log(name);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Review;
