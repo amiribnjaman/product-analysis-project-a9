41889242522a9270d64335c157bfba0369b2771b
amiribnjaman <amiribnjaman@gmail.com>
2022-04-04 05:45:21 +0600

    Blog page added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,32 +1,21 @@
 import React from 'react';
-import { Accordion } from 'react-bootstrap';
+// import { Accordion } from 'react-bootstrap';
 
 const Blog = () => {
     return (
-        <div className='container mt-5'>
+        <div className='container my-5'>
             <div className='row'>
                 <div className='col-md-6 offset-md-3'>
-                    <Accordion defaultActiveKey="0">
-                        <Accordion.Item eventKey="0">
-                            <Accordion.Header>What is Context Api?</Accordion.Header>
-                            <Accordion.Body>
-                                Context api is one of the most important hooks in react. It's produce global variables to use in any children components. Before this hooks, Developer pass the values using props drilling and that method was most complicated and time costing. But context api easy this case. Using the context api, developer can simply create a context api and can pass any type of data through api and take the value any where in the app where he needed.
-                            </Accordion.Body>
-                        </Accordion.Item>
-                        <Accordion.Item eventKey="1">
-                            <Accordion.Header>Accordion Item #2</Accordion.Header>
-                            <Accordion.Body>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                est laborum.
-                            </Accordion.Body>
-                        </Accordion.Item>
-                    </Accordion>
+                    <h3>What is Context Api?</h3>
+                    <p>Context api is one of the most important hook in react. It's produce global variables to use in any children components. Before this hooks, Developer pass the values using props drilling and that method was most complicated and time costing. But context api easy this case. Using the context api, developer can simply create a context api and can pass any type of data through api and take the value any where in the app where he needed.</p>
                 </div>
+                <div className='col-md-6 offset-md-3'>
+                    <h3>What are differences between Inline, Inline block and Block Elements?</h3>
+                    <p><strong>Inline Elements:</strong> Inline elements means, those elements no need full the line or dosen't block the full line rather this elements only contain those place its needed. Like: img, a, span etc. <br />
+                        <strong>Inline block:</strong> Inline block means, only contain its place but behave like block elements. <br />
+                        <strong>Block Elements:</strong> Block elements are- those elements containe or block full the line if it need or not its called block elements. Like: h1-h6, p, div etc.</p>
+                </div>
+                
             </div>
         </div>
     );
